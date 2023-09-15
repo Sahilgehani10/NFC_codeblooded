9020e0e1db7a9b4eb33f9aa9f61e945bb4071c16
Sahil Gurnani <gurnanisahil87@gmail.com>
2023-09-15 21:01:32 +0530

    Feedback form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
+import Feedback from "./Feedback";
 const App = () => {
   return (
     <div className="h-[1000px] relative">
       <Navbar />
+      <Feedback />
       <footer>
         <Footer />
       </footer>
