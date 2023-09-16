9afe3ad788dba4b5c09aecc5da62aa5b72f5352a
Eshaan Vaswani <eshaanv2004@gmail.com>
2023-09-16 08:00:29 +0530

    Revert "backend"
    This reverts commit c4920ece31a84c43c89f87a5b7110f87b73f5129.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
-// const app = require("./app");
+const app = require("./app");
 
-// const dotenv = require("dotenv");
-// const connectDatabase = require("./config/database");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 
-// //config
-// dotenv.config({path:"backend/config/config.env"})
+//config
+dotenv.config({path:"backend/config/config.env"})
 
-// connectDatabase()
+connectDatabase()
 
-// app.listen(process.env.PORT, ()=>{
-//     console.log(`Server is working on http://localhost:${process.env.PORT}`)
-// })
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is working on http://localhost:${process.env.PORT}`)
+})
+
