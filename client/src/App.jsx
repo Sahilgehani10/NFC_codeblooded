2ed1cbe7c50339d9905dc7cedec263721727615e
Sahil Gurnani <gurnanisahil87@gmail.com>
2024-02-18 18:35:48 +0530

    yo
    maps only

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,13 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Landing from "./Landing";
-import Maps from './pages/maps'
-import Layout from "./pages/Layout";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import Homepage from "./Homepage";
-import Features from "./Features";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Maps from "./pages/maps";
 
 const App = () => {
   return (
     <Routes>
-  
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Landing />}/>
-        <Route path="/features" element={<Features />} />
-        <Route path="/maps" element={<Maps />}/>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/register" element={<RegisterPage />}/>
-      </Route>
-      <Route path="/homepage" element={<Homepage />}/>
-      </Routes>
-   
+      <Route path="/maps" element={<Maps />} />
+    </Routes>
   );
 };
 
